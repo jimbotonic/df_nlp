44cd3e3a9e83c15db3c271e85c36ff2651f2d2b2
jimbotonic <jimmy.dubuisson@gmail.com>
2014-05-27 17:47:56 +0200

    v2

--- train_and_test_multi.py
+++ train_and_test_multi.py
@@ -1,5 +1,5 @@
 # 
-# Latassan is free software; you can redistribute it and/or
+# DF is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public
 # License as published by the Free Software Foundation;
 # either version 3 of the License, or (at your option) any
@@ -12,7 +12,7 @@
 # details.
 #
 # You should have received a copy of the GNU General Public
-# License along with Latassan; see the file COPYING.  If not
+# License along with DF; see the file COPYING.  If not
 # see <http://www.gnu.org/licenses/>.
 # 
 # Copyright (C) 2014 Jimmy Dubuisson <jimmy.dubuisson@gmail.com>
